2e3b975bc9112412fa040bc85fa4cbc60ad63989
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-04-11 22:12:47 +0200

    add servicies

--- springfantasy/src/main/java/com/example/springfantasy/repositories/CharacterRepository.java
+++ springfantasy/src/main/java/com/example/springfantasy/repositories/CharacterRepository.java
@@ -1,8 +1,16 @@
 package com.example.springfantasy.repositories;
 import com.example.springfantasy.entities.Character;
+import com.example.springfantasy.entities.enums.ClasseEnum;
+import com.example.springfantasy.entities.enums.RazzaEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CharacterRepository extends JpaRepository<Character,Long> {
+
+    List<Character> findByNome(String nome);
+    List<Character> findByRazzaEnum(RazzaEnum razzaEnum);
+    List<Character> findByClasseEnum(ClasseEnum classeEnum);
 }
--- springfantasy/src/main/java/com/example/springfantasy/services/CharacterService.java
+++ springfantasy/src/main/java/com/example/springfantasy/services/CharacterService.java
@@ -1,8 +1,14 @@
 package com.example.springfantasy.services;
 
+import com.example.springfantasy.entities.Character;
+import com.example.springfantasy.entities.enums.ClasseEnum;
+import com.example.springfantasy.entities.enums.RazzaEnum;
 import com.example.springfantasy.repositories.CharacterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CharacterService implements CharacterServiceInterface{
@@ -10,6 +16,65 @@ public class CharacterService implements CharacterServiceInterface{
     @Autowired
     private CharacterRepository characterRepository;
 
+    @Override
+    public Character createCharacter(Character character) {
+        Character savedCharacter = characterRepository.save(character);
+        return savedCharacter;
+    }
+    @Override
+    public List<Character> getAllCharacters() {
+        List<Character> characters = characterRepository.findAll();
+        return characters;
+    }
 
+    @Override
+    public Optional<Character> getCharacterById(Long id) {
+        Optional<Character> characterOPT = characterRepository.findById(id);
+        return characterOPT;
+    }
 
+    @Override
+    public Optional<Character> updateCharacter(Long id, Character character) {
+        Optional<Character> updatedCharacter = characterRepository.findById(id);
+        if (updatedCharacter.isPresent()){
+            updatedCharacter.get().setNome(character.getNome());
+            updatedCharacter.get().setRazzaEnum(character.getRazzaEnum());
+            updatedCharacter.get().setLivelloEnum(character.getLivelloEnum());
+            updatedCharacter.get().setClasseEnum(character.getClasseEnum());
+            updatedCharacter.get().setDescrizione(character.getDescrizione());
+            characterRepository.save(updatedCharacter.get());
+        } else {
+            return Optional.empty();
+        }
+        return updatedCharacter;
+    }
+
+    @Override
+    public Optional<Character> deleteCharacter(Long id) {
+        Optional<Character> deletedCharacterOPT = characterRepository.findById(id);
+        if(deletedCharacterOPT.isPresent()){
+            characterRepository.delete(deletedCharacterOPT.get());
+            return deletedCharacterOPT;
+        }else{
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Character> searchByName(String nome) {
+        List<Character> byName = characterRepository.findByNome(nome);
+        return byName;
+    }
+
+    @Override
+    public List<Character> searchByRazza(RazzaEnum razzaEnum) {
+        List<Character> byRazza = characterRepository.findByRazzaEnum(razzaEnum);
+        return byRazza;
+    }
+
+    @Override
+    public List<Character> searchByClasse(ClasseEnum classeEnum) {
+        List<Character> byClasse = characterRepository.findByClasseEnum(classeEnum);
+        return byClasse;
+    }
 }
