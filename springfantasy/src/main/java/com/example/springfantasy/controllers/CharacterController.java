ae4022db881edc18fcaa99d217b6dd61d2bc3245
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-04-11 22:13:05 +0200

    add controllers

--- springfantasy/src/main/java/com/example/springfantasy/controllers/CharacterController.java
+++ springfantasy/src/main/java/com/example/springfantasy/controllers/CharacterController.java
@@ -1,9 +1,72 @@
 package com.example.springfantasy.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springfantasy.entities.Character;
+import com.example.springfantasy.entities.enums.ClasseEnum;
+import com.example.springfantasy.entities.enums.RazzaEnum;
+import com.example.springfantasy.services.CharacterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/character")
+@RequestMapping("/game")
 public class CharacterController {
+
+    @Autowired
+    private CharacterService characterService;
+
+    @PostMapping("/characters")
+    public ResponseEntity<Character> addUser(@RequestBody Character character){
+       characterService.createCharacter(character);
+        return ResponseEntity.ok().body(character);
+    }
+
+    @GetMapping("/characters")
+    public ResponseEntity<List<Character>> getAllCharacters(){
+        List<Character> allCharacters = characterService.getAllCharacters();
+        return ResponseEntity.ok().body(allCharacters);
+    }
+
+    @GetMapping("/characters/{id}")
+    public ResponseEntity<Optional<Character>> getCharacter(@PathVariable Long id){
+        Optional<Character> characterOPT = characterService.getCharacterById(id);
+        return ResponseEntity.ok().body(characterOPT);
+    }
+
+    @PutMapping("/characters/{id}")
+    public ResponseEntity<Character> updateCharacterById(@RequestBody Character character,@PathVariable Long id){
+        Optional<Character> characterOPT = characterService.updateCharacter(id,character);
+        if(characterOPT.isPresent()){
+            return  ResponseEntity.ok().body(characterOPT.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/characters/{id}")
+    public ResponseEntity<Optional<Character>> deleteCharacterById(@PathVariable Long id){
+        Optional<Character> characterOPT = characterService.deleteCharacter(id);
+        if(characterOPT.isPresent()){
+            return ResponseEntity.ok().body(characterOPT);
+        }
+        return ResponseEntity.notFound().build();
+
+    }
+    @GetMapping("/characters/search-by-name")
+    public ResponseEntity<List<Character>> getCharacterByName(@RequestParam String nome){
+        List<Character> characterOPT = characterService.searchByName(nome);
+        return ResponseEntity.ok().body(characterOPT);
+    }
+    @GetMapping("/characters/search-by-race")
+    public ResponseEntity<List<Character>> getCharacterByRace(@RequestParam RazzaEnum razzaEnum){
+        List<Character> characterOPT = characterService.searchByRazza(razzaEnum);
+        return ResponseEntity.ok().body(characterOPT);
+    }
+    @GetMapping("/characters/search-by-class")
+    public ResponseEntity<List<Character>> getCharacterByClass(@RequestParam ClasseEnum classeEnum){
+        List<Character> characterOPT = characterService.searchByClasse(classeEnum);
+        return ResponseEntity.ok().body(characterOPT);
+    }
 }
--- springfantasy/src/main/java/com/example/springfantasy/services/CharacterServiceInterface.java
+++ springfantasy/src/main/java/com/example/springfantasy/services/CharacterServiceInterface.java
@@ -1,4 +1,21 @@
 package com.example.springfantasy.services;
 
+import com.example.springfantasy.entities.Character;
+import com.example.springfantasy.entities.enums.ClasseEnum;
+import com.example.springfantasy.entities.enums.RazzaEnum;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CharacterServiceInterface {
+
+    List<Character> getAllCharacters(); //Restituisce l'elenco completo dei personaggi.
+    Optional<Character> getCharacterById(Long id); //Restituisce un personaggio specifico per ID.
+    Character createCharacter(Character character); //Crea un nuovo personaggio.
+    Optional<Character> updateCharacter(Long id, Character character); //Aggiorna i dettagli di un personaggio esistente.
+	Optional<Character> deleteCharacter(Long id); //Rimuove un personaggio dall'elenco.
+    List<Character> searchByName(String nome); //Cerca personaggi per nome.
+    List<Character> searchByRazza(RazzaEnum razzaEnum); //Cerca personaggi per razza.
+    List<Character> searchByClasse(ClasseEnum classeEnum); //Cerca personaggi per classe.
+
 }
