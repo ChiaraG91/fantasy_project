0f5c6e588c77aa8fc63f1f51ee12fcd62bc21e45
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-04-11 17:24:47 +0200

    Add files via upload

---
+++ springfantasy/src/main/java/com/example/springfantasy/SpringfantasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfantasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfantasyApplication.class, args);
+	}
+
+}
---
+++ springfantasy/src/test/java/com/example/springfantasy/SpringfantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
